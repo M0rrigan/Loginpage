94449ade35d1f0a37cc54052b764aacec6bbefc1
S.Harish <93419618+M0rrigan@users.noreply.github.com>
2023-04-09 11:52:31 +0530

    Add files via upload

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:login_page/components/my_button.dart';
+import 'package:login_page/components/my_textfield.dart';
+import 'package:login_page/components/square_tile.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void signUserIn() {}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 50),
+              Icon(
+                Icons.lock,
+                size: 100,
+              ),
+              const SizedBox(height: 50),
+              Text(
+                'Welcome to SRM Rural Kart',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 25),
+              Text(
+                'One app for all local needs',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 50),
+              MyTextFeild(
+                controller: usernameController,
+                hintText: 'Phone number',
+                obscureText: false,
+              ),
+              const SizedBox(height: 15),
+              MyTextFeild(
+                controller: passwordController,
+                hintText: 'OTP',
+                obscureText: true,
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'forgot password?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 25),
+              MyButton(
+                onTap: signUserIn,
+              ),
+              const SizedBox(height: 50),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'Or continue with',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 50),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
